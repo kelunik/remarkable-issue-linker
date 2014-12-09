889d4314d3b68af7f4e3fd699101607ba14ea359
Niklas Keller <me@kelunik.com>
2014-12-09 23:30:28 +0100

    Fix test regression

--- test/test.js
+++ test/test.js
@@ -2,8 +2,9 @@ var remarkable = new (require("remarkable"))();
 remarkable.use(require("../lib/issue-linker")());
 
 var output = remarkable.render("There's a new issue at GitHub: https://github.com/jonschlinkert/remarkable/issues/117");
+var expect = '<p>There\'s a new issue at GitHub: <a href="https://github.com/jonschlinkert/remarkable/issues/117" target="_blank"><i class="fa fa-github"></i>jonschlinkert/remarkable#117</a></p>';
 
-if(output !== '<p>There\'s a new issue at GitHub: <a href="https://github.com/jonschlinkert/remarkable/issues/117" target="_blank"><i class="fa fa-github"></i>jonschlinkert/remarkable#117</a>17</p>') {
+if(output !== expect) {
 	console.log(output);
 	process.exit(1);
 }
